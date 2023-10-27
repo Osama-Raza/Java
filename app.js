29cbbabb3a1875b85e496b77957a5fdb65896a3c
Osama-Raza <osamaraza988@gmail.com>
2023-10-28 01:41:33 +0500

    first comit

---
+++ app.js
@@ -0,0 +1,34 @@
+var x = (3)
+var y = (5)
+document.write("Sum of 3 and 5 is ");
+document.write ( x+y)
+document.write(" <br> Subtracat 3 from 5 is ");
+document.write (x-y)
+document.write(" <br> Multiply 3 by 5 is ");
+document.write(x*y);
+document.write(" <br> Divide 3 by 5 is ");
+document.write(x/y);
+document.write(" <br> Modulus 3 of 5 is ");
+document.write(x%y);
+var a = (2)
+var b = (3)
+document.write(" <br> <br>Initial value:");
+document.write(a+b);
+document.write(" <br> Value after increment: ");
+document.write(++a + b);
+var c = (7)
+document.write("<br> Value after addition:  ");
+document.write(a+b+c);
+document.write("<br> Value after decrement:  ");
+document.write(a+b+ --c);
+document.write("<br> The remainder is :  ")
+document.write ((a+b+ c--) % 3)
+var ticket = (600)
+var quantity = (5)
+
+document.write(" <br> <br>Total cost to buy 5 tickets to a movie is Rs.");
+document.write (ticket*quantity);
+
+var table = (4)
+document.write(" <br> <br>Table of 4");
+document.write();
